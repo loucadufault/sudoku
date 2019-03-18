f2dcb86790b048765f9984a1aebc2d6520b931a5
Louca Dufault <louca.dufault@gmail.com>
2019-03-18 09:38:51 -0400

    finished and working

--- sudoku.py
+++ sudoku.py
@@ -1,8 +1,9 @@
 import time
 
-digits = list(range(10))
+digits = list(range(1,10))
 letters = 'abcdefghi'
-boxes = {'a':(0,0), 'b':(3,0), 'c':(6,0), 'd':(0,3), 'e':(3,3), 'f':(6,3), 'g':(0,6), 'h':(3,6), 'i':(6,6)}
+boxes = {'tl':(0,0), 'tm':(3,0), 'tr':(6,0), 'ml':(0,3), 'mm':(3,3), 'mr':(6,3), 'bl':(0,6), 'bm':(3,6), 'br':(6,6)}
+total_iters = 0
 
 #		 0 1 2 3 4 5 6 7 8
 input =[[5,1,7,6,0,0,0,3,4], #0
@@ -15,68 +16,102 @@ input =[[5,1,7,6,0,0,0,3,4], #0
 		[7,0,3,4,0,0,5,6,0], #7
 		[0,0,0,0,0,0,0,0,0]] #8
 
-cells = []
+sudoku = []
 
-def view_possibles():
+def view_candidates():
+	global total_iters
 	for i_row in range(9):
 		for i_col in range(9):
-			cell = cells[i_row][i_col]
-			print(str(cell) + ": " + str(cell.possibles))
+			cell = sudoku[i_row][i_col]
+			#print('at cell ({}, {})'.format(i_col, i_row))
+			#print(str(cell) + " candidates: " + str(cell.candidates))
+			#print(str(cell) + " value: " + str(cell.value))
+			#print(str(cell) + " box: " + str(cell.box) + '\n')
+			total_iters+=1
 
-def create_cells(input):
+def create_sudoku(input):
+	global total_iters
 	for i_row in range(9):
-		cells.append([])
+		sudoku.append([])
 		for i_col in range(9):
 			cell = Cell(i_col, i_row, input[i_row][i_col])
-			cells[i_row].append(cell)
+			sudoku[i_row].append(cell)
+			total_iters+=1
 
 def elim_from_col(col, value):
+	global total_iters
 	for i_row in range(9):
-		cells[i_row][col].remove_possible(value)
+		cell = sudoku[i_row][col]
+		if cell.value == 0:
+			#print('cell {} does not have a value yet'.format(cell))
+			cell.remove_candidate(value)
+		total_iters+=1
 
 def elim_from_row(row, value):
+	global total_iters
 	for i_col in range(9):
-		cells[row][i_col].remove_possible(value)	
+		cell = sudoku[row][i_col]
+		if cell.value == 0:
+			cell.remove_candidate(value)
+		total_iters+=1	
 
 def elim_from_box(box, value):
+	global total_iters
 	coords = boxes[box]
 	col = coords[0] #3
 	row = coords[1] #0
 	for i_row in range(row, row+3):
 		for i_col in range(col, col+3):
-			cells[i_row][i_col].remove_possible(value)
+			#print(sudoku[i_row][i_col])
+			cell = sudoku[i_row][i_col]
+			if cell.value == 0:
+				cell.remove_candidate(value)
+			total_iters+=1
 
 class Cell():
 	def __init__(self, col, row, value):
-		self.value = value
+		if value == 0:
+			self.value = 0
+			self.candidates = digits[:] #ffs
+		else:
+			self.value = value
+			self.candidates = [value]
+
 		self.col = col
 		self.row = row
 		self.box = self.get_box()
-		self.possibles = digits
 
 	def __str__(self):
 		return "{}{}".format(letters[self.col], self.row+1)
 
+	def is_empty(self):
+		return self.value == 0
+
 	def set_value(self, value):
+		draw(sudoku)
 		self.value = value
 
 	def get_box(self):
+		global total_iters
 		for key, val in boxes.items():
 			if ((self.col >= val[0]) and (self.col < val[0]+3)) and ((self.row >= val[1]) and (self.row < val[1]+3)):
 				return key
+			total_iters+=1
 
-	def remove_possible(self, value):
-		if value in self.possibles:
-			(self.possibles).remove(value) #prolly wrong
-			print('value removed')
-		else:
-			print('no value removed')
-		if len(self.possibles) == 1:
-			self.set_value(self.possibles[0])
-			self.propagate()
-			print('finished propagation for this cell')
-		# elif len(self.possibles) < 1:
-		# 	print('impossible')
+	def remove_candidate(self, value):
+		#print('trying to remove {} from cell {}'.format(value, self))
+		if self.is_empty():
+			#print(self.candidates)
+			if value in self.candidates:
+				(self.candidates).remove(value) #prolly wrong
+				#print('value removed')
+			#else:
+				#print('no value removed')
+
+			if len(self.candidates) == 1:
+				self.set_value(self.candidates[0])
+				self.propagate()
+				#print('finished propagation for cell: ', self)
 
 	def propagate(self):
 		elim_from_col(self.col, self.value)
@@ -84,33 +119,40 @@ class Cell():
 		elim_from_box(self.box, self.value)
 
 	def elim_by_col(self):
+		global total_iters
 		for i_row in range(9):
-			compare = cells[i_row][self.col]
-			self.remove_possible(compare.value)
+			compare = sudoku[i_row][self.col]
+			if not compare.is_empty():
+				self.remove_candidate(compare.value)
+			total_iters+=1
 
 	def elim_by_row(self):
+		global total_iters
 		for i_col in range(9):
-			compare = cells[self.row][i_col]
-			self.remove_possible(compare.value)
+			compare = sudoku[self.row][i_col]
+			if not compare.is_empty():
+				self.remove_candidate(compare.value)
+			total_iters+=1
 
 	def elim_by_box(self):
-		draw(cells)
-		print(self.box)
+		global total_iters
 		coords = boxes[self.box]
 		col = coords[0] #3
 		row = coords[1] #0
 		for i_row in range(row, row+3):
 			for i_col in range(col, col+3):
-				compare = cells[i_row][i_col]
-				self.remove_possible(compare.value)
+				compare = sudoku[i_row][i_col]
+				if not compare.is_empty():
+					self.remove_candidate(compare.value)
+				total_iters+=1
 
-def draw(cells):
+def draw(sudoku):
 	horizon_div = '------+-------+------'
 	print('    A B C   D E F   G H I\n')
 	for i_row in range(9):
 		print(str(i_row+1) + '  ', end='')
 		for i_col in range(9):
-			cell = cells[i_row][i_col]
+			cell = sudoku[i_row][i_col]
 			#print(' {}'.format(cell.box), end='')
 			print(' {}'.format(cell.value if cell.value else ' '), end='')
 			if (i_col==2 or i_col==5):
@@ -118,32 +160,41 @@ def draw(cells):
 		if (i_row==2 or i_row==5):
 			print('\n    '+horizon_div, end='')
 		print('')
+	print('')
 
-def is_solved(cells):
+def is_solved(sudoku):
+	global total_iters
 	for i_row in range(9):
 		for i_col in range(9):
-			cell = cells[i_row][i_col]
-			if not (cell.value):
+			cell = sudoku[i_row][i_col]
+			if cell.is_empty():
 				return False
+			total_iters+=1
 	return True
 
 def basic():
+	global total_iters
 	for i_row in range(9):
 		for i_col in range(9):
-			cell = cells[i_row][i_col]
-			cell.elim_by_col()
-			cell.elim_by_row()
-			cell.elim_by_box()
+			cell = sudoku[i_row][i_col]
+			#print('basic at cell:', cell)
+			if cell.is_empty():
+				cell.elim_by_col()
+				cell.elim_by_row()
+				cell.elim_by_box()
+			total_iters+=1
 
 def main():
-	create_cells(input)
-	draw(cells)
-	while not is_solved(cells):
-		view_possibles()
-		time.sleep(10)
+	global total_iters
+	create_sudoku(input)
+	draw(sudoku)
+	time.sleep(1)
+	while not is_solved(sudoku):
+		#view_candidates()
 		basic()
-		draw(cells)
-	print('solved')
+		draw(sudoku)
+		total_iters+=1
+	print('solved after {} iterations'.format(total_iters))
 
 if __name__=='__main__':
 	main()	
